84520fc2aef6c37fcc7bb67c7831a586a6188bb3
Daniel Miller <daniel.miller.djm@gmail.com>
2019-07-20 00:45:22 +1200

    dijkstras implementation bugfixed (wall index off by 1) and converted into static class

--- FlowField/Assets/Scenes/Scripts/FlowFieldGrid.cs
+++ FlowField/Assets/Scenes/Scripts/FlowFieldGrid.cs
@@ -2,44 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlowFieldGrid : MonoBehaviour {
-
-    public int gridSizeX;
-    public int gridSizeY;
-    public FlowFieldTile target;
-    public List<Vector2Int> walls;
-    private FlowFieldTile[,] weightArray;
+public static class FlowFieldGrid {
 
 
-    // Start is called before the first frame update
-    public FlowFieldGrid() {
-        this.gridSizeX = 10;
-        this.gridSizeY = 10;
-        //GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        this.target = new FlowFieldTile(5, 3);
-        this.walls = new List<Vector2Int>();
-        this.walls.Add(new Vector2Int(2,4));
-        this.weightArray = generateDijkstraGrid(this.gridSizeX, this.gridSizeY, this.walls);
-    }
+    public static FlowFieldTile[,] generateDijkstraGrid(int gridSizeX, int gridSizeY, FlowFieldTile target, List<Vector2Int> walls) {
 
 
-
-    private FlowFieldTile[,] generateDijkstraGrid(int gridWidth, int gridHeight, List<Vector2Int> walls/*ArrayList of vector2dInt*/) {
-        //Generate an empty grid, set all places as weight -1, which will stand for unvisited
-        FlowFieldTile[,] dijkstraGrid = new FlowFieldTile[gridWidth, gridHeight];
-        for (int x = 0; x < gridWidth; x++) {
-            for (int y = 0; y < gridHeight; y++) {
-                dijkstraGrid[x,y] = new FlowFieldTile(x, y);
+    //Generate an empty grid, set all places as weight -1, which will stand for unvisited
+    FlowFieldTile[,] dijkstraGrid = new FlowFieldTile[gridSizeX, gridSizeY];
+        for (int x = 0; x < gridSizeX; x++) {
+            for (int y = 0; y < gridSizeY; y++) {
+                dijkstraGrid[x, y] = new FlowFieldTile(x, y);
             }
         }
 
         //Set all places where obstacles are as being weight MaxValue, which will stand for not able to go here
-        foreach (Vector2Int element in walls){
-            dijkstraGrid[element.x, element.y].setWeight(int.MaxValue);
+        foreach (Vector2Int element in walls) {
+            if (element.x < 0 || element.x > gridSizeX-1 || element.y<0 || element.y > gridSizeX-1) {
+                Debug.Log("BAD WALL VECTOR- x: " + element.x + " y: " + element.y);
+            }
+            else {
+                dijkstraGrid[element.x, element.y].setWeight(int.MaxValue);
+            }
         }
 
         //flood fill out from the end point
-        FlowFieldTile destination = this.target;
+        FlowFieldTile destination = target;
         destination.setWeight(0);
         dijkstraGrid[destination.getVector2d().x, destination.getVector2d().y].setWeight(0);
 
@@ -48,16 +36,15 @@ public class FlowFieldGrid : MonoBehaviour {
 
 
         //for each node we need to visit, starting with the pathEnd
-        //foreach (FlowFieldTile node in toVisit.ToArray()) {//fuck me
-        for(int i =0; i<toVisit.Count; i++) {
+        for (int i = 0; i < toVisit.Count; i++) {
 
-            List<FlowFieldTile> neighbours = straightNeighboursOf(toVisit[i]);
+            List<FlowFieldTile> neighbours = straightNeighboursOf(toVisit[i], gridSizeX, gridSizeY);
 
             //for each neighbour of this node (only straight line neighbours, not diagonals)
             foreach (FlowFieldTile neighbour in neighbours) {
 
                 //We will only ever visit every node once as we are always visiting nodes in the most efficient order
-                if (dijkstraGrid[neighbour.getVector2d().x, neighbour.getVector2d().y].getWeight()==-1) {//if tile has not been visited
+                if (dijkstraGrid[neighbour.getVector2d().x, neighbour.getVector2d().y].getWeight() == -1) {//if tile has not been visited
                     neighbour.setWeight(toVisit[i].getWeight() + 1);
                     dijkstraGrid[neighbour.getVector2d().x, neighbour.getVector2d().y].setWeight(neighbour.getWeight());
                     toVisit.Add(neighbour);
@@ -69,7 +56,7 @@ public class FlowFieldGrid : MonoBehaviour {
 
 
 
-    private List<FlowFieldTile> straightNeighboursOf(FlowFieldTile tile) {
+    private static List<FlowFieldTile> straightNeighboursOf(FlowFieldTile tile, int gridSizeX, int gridSizeY) {
         List<FlowFieldTile> neighbours = new List<FlowFieldTile>();
         if (tile.getVector2d().x > 0) {
             neighbours.Add(new FlowFieldTile(tile.getVector2d().x - 1, tile.getVector2d().y));
@@ -77,20 +64,12 @@ public class FlowFieldGrid : MonoBehaviour {
         if (tile.getVector2d().y > 0) {
             neighbours.Add(new FlowFieldTile(tile.getVector2d().x, tile.getVector2d().y - 1));
         }
-
         if (tile.getVector2d().x < gridSizeX - 1) {
             neighbours.Add(new FlowFieldTile(tile.getVector2d().x + 1, tile.getVector2d().y));
         }
         if (tile.getVector2d().y < gridSizeY - 1) {
             neighbours.Add(new FlowFieldTile(tile.getVector2d().x, tile.getVector2d().y + 1));
         }
-
         return neighbours;
     }
-
-
-
-
-
-
 }
---
+++ FlowField/Assets/Scenes/Scripts/temp.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temp : MonoBehaviour {
+    private int gridSizeX=5;
+    private int gridSizeY=10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FlowFieldTile[,] arr = FlowFieldGrid.generateDijkstraGrid(gridSizeX, gridSizeY, getTarget(), getWalls());
+
+        string s = "";
+        for(int i = 0; i < gridSizeX; i++) {
+            for(int y = 0; y < gridSizeY; y++) {
+
+                if (arr[i, y].getWeight() == int.MaxValue) {
+                    s = s + "X";
+                }
+                else {
+                    s = s + arr[i, y].getWeight().ToString();
+                }
+            }
+            s = s + "\n";
+        }
+        Debug.Log(s);
+    }
+
+ 
+
+
+    private FlowFieldTile getTarget() {
+        return new FlowFieldTile(3, 3);
+    }
+
+    private List<Vector2Int> getWalls() {
+        List<Vector2Int> walls = new List<Vector2Int>();
+        walls.Add(new Vector2Int(0, 2));
+        walls.Add(new Vector2Int(4, 3));
+        return walls;
+    }
+
+
+}
