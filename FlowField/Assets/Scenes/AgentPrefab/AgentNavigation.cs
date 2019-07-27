1e01a26f502d091c9520533987fcbd215482521d
Daniel Miller <daniel.miller.djm@gmail.com>
2019-07-27 17:16:04 +1200

    added maze model into scene, applied janky fix to stop agents getting stuck on walls

--- FlowField/Assets/Scenes/AgentPrefab/AgentNavigation.cs
+++ FlowField/Assets/Scenes/AgentPrefab/AgentNavigation.cs
@@ -8,30 +8,36 @@ public class AgentNavigation : MonoBehaviour
     public Transform agentPosition;
     public Rigidbody rb;
     public float force =1.0f;
-    private Vector3 backupMovement = Vector3.zero;
+
+    private DijkstraTile lastValidTile;
+
     private void Start() {
-        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        //rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+        //this.lastValidTile = worldGrid.NodeFromWorldPoint(agentPosition.position);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         
-        Vector2Int vec = worldGrid.NodeFromWorldPoint(agentPosition.position).getFlowFieldVector();
-        Vector3 moveDir = new Vector3(vec.x, 0, vec.y).normalized;
+        DijkstraTile currentTile = worldGrid.NodeFromWorldPoint(agentPosition.position);
+        if(this.lastValidTile == null) { this.lastValidTile = currentTile; }
+        if (currentTile.getFlowFieldVector().Equals(Vector2Int.zero)) {
 
-        //if (moveDir.Equals(Vector3.zero)) {
-        //    if (this.backupMovement.Equals(Vector3.zero)) {
-        //        Debug.Log("Agent started on invalid tile");
-        //    }
-        //    rb.AddForce(-this.backupMovement * Time.deltaTime * force, ForceMode.Force);
-        //}
-        //else {
-        //    this.backupMovement = moveDir;
-        //    rb.AddForce(moveDir * Time.deltaTime * force, ForceMode.Force);
-        //}
+            
+            Vector2Int flowVector = this.lastValidTile.getVector2d() - currentTile.getVector2d();
+            Vector3 moveDir = new Vector3(flowVector.x, 0, flowVector.y).normalized;
+            rb.AddForce(moveDir * Time.deltaTime * force, ForceMode.Force);
+            //transform.position += moveDir * Time.deltaTime;
 
-        transform.position += moveDir * force * Time.deltaTime;
+        }
+        else {
+            this.lastValidTile = currentTile;
+            Vector2Int flowVector = currentTile.getFlowFieldVector();
+            Vector3 moveDir = new Vector3(flowVector.x, 0, flowVector.y).normalized;
+            rb.AddForce(moveDir * Time.deltaTime * force, ForceMode.Force);
 
+        }
     }
 }
